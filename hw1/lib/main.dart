a7d0dd36aa9089bff43c4eb0699e5e7f32bdcc2a
Andrei Draghici <draghici.andrei98@gmail.com>
2023-04-04 19:29:05 +0300

    added picture top

--- hw1/lib/main.dart
+++ hw1/lib/main.dart
@@ -1,20 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main(){
+void main() {
   runApp(const CurrencyConvertor());
 }
 
 class CurrencyConvertor extends StatelessWidget {
   const CurrencyConvertor({Key? key}) : super(key: key);
-  final _imagePath ='lib/assets/ron.jpg';
+  final _imagePath = 'lib/assets/ron.jpg';
 
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
       home: Convert(),
-
     );
   }
 }
@@ -24,142 +23,108 @@ class Convert extends StatefulWidget {
 
   @override
   State<Convert> createState() => _ConvertState();
-
 }
 
 class _ConvertState extends State<Convert> {
-
   bool _is_ron = true;
   double _input_ammount = 0;
   double _output_ammout = 0;
   double _eurron = 4.5;
-  String _output ="0.00EURO";
+  String _output = "0.00EURO";
   String _input_name = "RON";
 
   final TextEditingController input_val = TextEditingController();
-  @override
 
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Currency Convertor"),
       ),
-
-      body:
-      Container(
+      body: Container(
         alignment: Alignment.center,
-
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:    [
-                  SizedBox(
-                    width: 150,
-                    child: TextField(
-                      controller: input_val,
-                      decoration: InputDecoration(
-                        labelText: _input_name,
-
+            const Image(
+                image: ResizeImage(AssetImage('ron.jpg'),
+                    width: 400, height: 250)),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        child: TextField(
+                          controller: input_val,
+                          decoration: InputDecoration(
+                            labelText: _input_name,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-
-                  GestureDetector(
-                    onTap: (){
-                      print("TAP");
-                      if( _is_ron == false){
-                        _input_name = "RON";
-                        _output = "EURO";
-                        _is_ron = true;
-                      }
-                      else{
-                        _input_name = "EURO";
-                        _output = "RON";
-                        _is_ron = false;
-                      }
-                      setState(() {
-
-                      });
-                    },
-                    child: Icon(
-                        Icons.arrow_circle_right_outlined
-                    ),
-                  ),
-
-
-                  SizedBox(
-                    width: 200,
-
-                    child: Text(_output,
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontSize:30,
+                      GestureDetector(
+                        onTap: () {
+                          if (_is_ron == false) {
+                            _input_name = "RON";
+                            _output = "EURO";
+                            _is_ron = true;
+                          } else {
+                            _input_name = "EURO";
+                            _output = "RON";
+                            _is_ron = false;
+                          }
+                          setState(() {});
+                        },
+                        child: const Icon(Icons.arrow_circle_right_outlined),
                       ),
-                    ),
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          _output,
+                          textAlign: TextAlign.right,
+                          style: const TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
+                    ]),
+                const SizedBox(height: 35),
+                GestureDetector(
+                  onTap: () {
+                    try {
+                      _input_ammount = double.parse(input_val.text);
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Please enter a valid number")));
+                    }
+                    if (_is_ron == true) {
+                      _output_ammout = _input_ammount / _eurron;
+                      _output = ("${_output_ammout.toStringAsFixed(2)}EURO");
+                    } else {
+                      _output_ammout = _input_ammount * _eurron;
+                      _output = ("${_output_ammout.toStringAsFixed(2)}RON");
+                    }
+
+                    setState(() {});
+                  },
+                  child: const Text(
+                    "CONVERT",
+                    style: TextStyle(
+                        fontSize: 30,
+                        //height: 4,
+                        color: Colors.blue,
+                        backgroundColor: Colors.yellow),
                   ),
-                ]
-            ),
-            const SizedBox(height: 35),
-
-            GestureDetector(
-              onTap: (){
-                print("TAP");
-
-
-
-                try {
-                  _input_ammount = double.parse(input_val.text);
-                }catch(e){
-                  print("INPUT INVALID");
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text(
-                      "Please enter a valid number"
-                    )
-                    )
-                  );
-
-                }
-                if( _is_ron == true){
-                  _output_ammout = _input_ammount / _eurron;
-                  _output = ("${_output_ammout.toStringAsFixed(2)}EURO");
-
-                }else
-                {
-                  _output_ammout = _input_ammount * _eurron;
-                  _output = ("${_output_ammout.toStringAsFixed(2)}RON");
-                }
-
-
-                setState(() {
-
-                });
-              },
-              child: const Text("CONVERT",
-                style: TextStyle(
-                    fontSize:30,
-                    //height: 4,
-                    color: Colors.blue,
-                    backgroundColor: Colors.yellow
                 ),
-              ),
+              ],
             ),
-
+            const SizedBox(height: 0),
           ],
-
         ),
-
       ),
-
     );
-
-
-
   }
 }
-
-
