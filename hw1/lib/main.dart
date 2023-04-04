ca2ad70145c0ba74924857229047bfcb062e1bd7
Andrei Draghici <draghici.andrei98@gmail.com>
2023-04-04 17:04:18 +0300

    added hw1
    currency convertor for EURO - RON

---
+++ hw1/lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main(){
+  runApp(const CurrencyConvertor());
+}
+
+class CurrencyConvertor extends StatelessWidget {
+  const CurrencyConvertor({Key? key}) : super(key: key);
+  final _imagePath ='lib/assets/ron.jpg';
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp(
+      home: Convert(),
+
+    );
+  }
+}
+
+class Convert extends StatefulWidget {
+  const Convert({Key? key}) : super(key: key);
+
+  @override
+  State<Convert> createState() => _ConvertState();
+
+}
+
+class _ConvertState extends State<Convert> {
+
+  bool _is_ron = true;
+  double _input_ammount = 0;
+  double _output_ammout = 0;
+  double _eurron = 4.5;
+  String _output ="0.00EURO";
+  String _input_name = "RON";
+
+  final TextEditingController input_val = TextEditingController();
+  @override
+
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Currency Convertor"),
+      ),
+
+      body:
+      Container(
+        alignment: Alignment.center,
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:    [
+                  SizedBox(
+                    width: 150,
+                    child: TextField(
+                      controller: input_val,
+                      decoration: InputDecoration(
+                        labelText: _input_name,
+
+                      ),
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: (){
+                      print("TAP");
+                      if( _is_ron == false){
+                        _input_name = "RON";
+                        _output = "EURO";
+                        _is_ron = true;
+                      }
+                      else{
+                        _input_name = "EURO";
+                        _output = "RON";
+                        _is_ron = false;
+                      }
+                      setState(() {
+
+                      });
+                    },
+                    child: Icon(
+                        Icons.arrow_circle_right_outlined
+                    ),
+                  ),
+
+
+                  SizedBox(
+                    width: 200,
+
+                    child: Text(_output,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize:30,
+                      ),
+                    ),
+                  ),
+                ]
+            ),
+            const SizedBox(height: 35),
+
+            GestureDetector(
+              onTap: (){
+                print("TAP");
+
+
+
+                try {
+                  _input_ammount = double.parse(input_val.text);
+                }catch(e){
+                  print("INPUT INVALID");
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text(
+                      "Please enter a valid number"
+                    )
+                    )
+                  );
+
+                }
+                if( _is_ron == true){
+                  _output_ammout = _input_ammount / _eurron;
+                  _output = ("${_output_ammout.toStringAsFixed(2)}EURO");
+
+                }else
+                {
+                  _output_ammout = _input_ammount * _eurron;
+                  _output = ("${_output_ammout.toStringAsFixed(2)}RON");
+                }
+
+
+                setState(() {
+
+                });
+              },
+              child: const Text("CONVERT",
+                style: TextStyle(
+                    fontSize:30,
+                    //height: 4,
+                    color: Colors.blue,
+                    backgroundColor: Colors.yellow
+                ),
+              ),
+            ),
+
+          ],
+
+        ),
+
+      ),
+
+    );
+
+
+
+  }
+}
+
+
